a330f3261e51288f8a0829b3ae300084f638b590
Brit Butler <redline6561@gmail.com>
2016-06-27 12:00:25 -0400

    Working but with a dup request bug. Damn you asynchrony!

--- src/js/main.js
+++ src/js/main.js
@@ -4,22 +4,37 @@ var movieList = $(".movies");
 var baseURL = "http://swapi.co/api/";
 var count = 1;
 
+function logIt (data, status, xhr) {
+  console.log(data);
+  console.log(status);
+  console.log(xhr);
+};
+
 function getPlanet (id) {
   var results = $.ajax({
-    url: baseURL + "planets/" + id + "/"
+    url: baseURL + "planets/" + id + "/",
+    dataType: 'json',
+    success: addPlanetToPage,
+    error: logIt
   });
-  console.log(results);
-  console.log(results.responseJSON);
-  return results.responseJSON;
-}
+  // setTimeout(function () {
+  //   console.log("synchronous log: " + results);
+  //   console.log("synchronous json log: " + results.responseJSON);
+  // }, 2000);
+  console.log("the request is running");
+};
+
+function addPlanetToPage (data) {
+  count++;
+  console.log(count, data);
+  $(".planets").append(planetTemplate(data));
+};
 
 function planetTemplate (planet) {
   return `<li class="planet">${planet.name} is pretty ${planet.climate}</li>`;
 };
 
 var button = $(".get-planet").on('click', function (event) {
-  var planet = getPlanet(count);
-  count++;
-  console.log(count);
-  $(".planets").append(planetTemplate(planet));
+  event.preventDefault();
+  getPlanet(count);
 });
